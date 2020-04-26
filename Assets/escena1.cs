3598be7808dd903f5b27376db12fd5830811b863
Jean <jean.canevello@gmail.com>
2020-04-26 16:02:34 -0500

    load project

---
+++ Assets/escena1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class escena1 : MonoBehaviour
+{
+	public void cambiarescena1(string nombreescena)
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void cambiarescena2(string nombreescena)
+	{
+		SceneManager.LoadScene(2);
+	}
+	public void cambiarescena3(string nombreescena)
+	{
+		SceneManager.LoadScene(3);
+	}
+	public void cambiarescena4(string nombreescena)
+	{
+		SceneManager.LoadScene(4);
+	}
+}
